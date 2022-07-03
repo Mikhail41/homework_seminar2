b898238fb4f856eec04f1a1df80b7381e5b9dc39
Mikhail <mikhail-zmyslin@yandex.ru>
2022-07-03 18:54:40 +0300

    решение первой задачи

---
+++ task001_siminar2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int shownumber()
+{
+
+    Console.WriteLine("введите трехзачное число :");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int second_num = num / 10 % 10;
+    Console.WriteLine($"второй цифрой числа {num} являеться {second_num}");
+    return (second_num);
+}
+Console.WriteLine(shownumber());
